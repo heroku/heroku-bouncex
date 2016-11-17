05e1fa3d770f8f334758cb23b0a7f4f32c440635
Alex Dovenmuehle <adoven@heroku.com>
2016-11-17 10:55:26 -0500

    Clean up unnecessary config.

--- config/dev.exs
+++ config/dev.exs
@@ -1,8 +1,3 @@
 use Mix.Config
 
-config :heroku_bouncex, Bouncer, 
-  encryption_salt: "COBuwSQv8yAMvsneF_8OgTsf_M3RFwfvLBNUbv2lD2vUN2E_-d81fYrelqryMejE",
-  signing_salt: "CVZ4WUrk__EzDDsBmyPjowuhh07pxGHjQQsy-2cKDHv_kq4Tj-YYNcaOKLrqS2gv",
-  secret_key_base: "yxKZ8NnIivFcgscvISCynPz7xZT24UW4PnZOy2Yh3iX8hCPa48N0BVHUbAx-Tgoy"
-
 config :heroku_bouncex, :strategy, Heroku.Bouncex.OAuthStrategy
